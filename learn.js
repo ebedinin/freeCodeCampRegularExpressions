bf8e32f7cfda792f491bece9901251b79e5de907
Admin <bedinin@gmail.com>
2021-11-18 17:18:25 +1000

    42

--- learn.js
+++ learn.js
@@ -1,5 +1,3 @@
-// Only change code below this line
-let chewieRegex = /Aa*/; // Change this line
-// Only change code above this line
-let chewieQuote = "Aaaaaaaaaaaaaaaarrrgh!"
-let result = chewieQuote.match(chewieRegex);
+let text = "<h1>Winter is coming</h1>";
+let myRegex = /<.*?>/; // Change this line
+let result = text.match(myRegex);
