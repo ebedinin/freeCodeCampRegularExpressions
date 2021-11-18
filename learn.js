05da5153265d17076f9e94f7f5def6607474e1e2
Admin <bedinin@gmail.com>
2021-11-18 16:45:52 +1000

    36

--- learn.js
+++ learn.js
@@ -1,3 +1,3 @@
-let quoteSample = "3 blind mice.";
-let myRegex = /[^0-9|^aeiou]/ig; // Change this line
-let result = quoteSample.match(myRegex); // Change this line
+let difficultSpelling = "Mississippi";
+let myRegex = /s+/g; // Change this line
+let result = difficultSpelling.match(myRegex);
